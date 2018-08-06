10028dccff93d9669fb679917f8ed192f753c6b5
Pablo Molina <pablomolina150895@gmail.com>
2018-08-06 13:08:14 +0200

    crear los modelos yy la base de datos de los productos

---
+++ HardwareShop/Models/DataContextProducts.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareShop.Models
+{
+    public class DataContextProducts
+    {
+        private IConfigurationRoot configuration;
+
+        public string ConnectionString { get; set; }
+
+        public DataContextProducts(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public DataContextProducts(IConfigurationRoot configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> list = new List<Product>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Product", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Product()
+                        {
+                            Id = reader.GetInt32("id"),
+                            Usuario = reader.GetString("usuario"),
+                            Contraseña = reader.GetString("contraseña")
+
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
+}
