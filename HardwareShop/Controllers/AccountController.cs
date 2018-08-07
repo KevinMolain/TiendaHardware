dda860da6cae86ee9922705203b6c23d60983753
Pablo Molina <pablomolina150895@gmail.com>
2018-08-07 09:45:16 +0200

    Pablo 0708mañana

--- HardwareShop/Controllers/AccountController.cs
+++ HardwareShop/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using HardwareShop.Helpers;
 using HardwareShop.Models;
@@ -52,6 +54,12 @@ namespace HardwareShop.Controllers
         public IActionResult Add()
         {
             return View("Add");
+            
+        }
+
+        public IActionResult Verify()
+        {
+
         }
 
         [Route("add")]
@@ -71,6 +79,45 @@ namespace HardwareShop.Controllers
             db.SetAccount(nuevaCuenta);
             return RedirectToAction("index");
             //LO MISMO, ESTA VISTA HABRÁ QUE MODIFICARLA
+        }
+
+        [Route("remember")]
+        [HttpPost]
+        public IActionResult Remember(string usuario)
+        {
+            // Leer de base de datos
+            DataContextUsers db = HttpContext.RequestServices.GetService(typeof(DataContextUsers)) as DataContextUsers;
+            List<Account> listaUsuarios = db.GetAllAccounts();
+
+            // servidor SMTP
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("hiberusclaseaspcoremvc@gmail.com", "111??aaa");
+            client.EnableSsl = true;
+
+            // 
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("hiberusclaseaspcoremvc@gmail.com");
+            foreach(Account a in listaUsuarios)
+            {
+                if (a.Usuario == usuario)
+                {
+                    mailMessage.To.Add(a.Correo);
+                    mailMessage.Body = "La contraseña que tenías era" + a.Contraseña + "a ver si no la vuelves a olvidar melón";
+                }               
+            }            
+            mailMessage.Subject = "Olvidación de la contraseña";
+
+            string output = "enviado";
+            try
+            {
+                client.Send(mailMessage);
+            }
+            catch (Exception e) { output = e.ToString() + "no enviado"; }
+
+            ViewBag.message = output;
+            return View("Success");
         }
     }
 }
