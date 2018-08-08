8197e5a7a5e104bab21943e8a82e96315420c9fa
MiesCode <41576777+Mikelesc@users.noreply.github.com>
2018-08-08 20:39:05 +0200

    Mejorado

--- HardwareShop/Controllers/CheckoutController.cs
+++ HardwareShop/Controllers/CheckoutController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HardwareShop.Helpers;
 using HardwareShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,13 +12,27 @@ namespace HardwareShop.Controllers
     public class CheckoutController : Controller
     {
         [Route("checkout")]
-        public IActionResult Checkout(int id)
+        public IActionResult Checkout(int id, string usuario)
         {
             if (id == 0)
             {
                 return View("../Account/Login");
             }
-            return View("Checkout");
+            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            Factura factura = new Factura();
+            factura.Items = cart;
+            DateTime FechaActual =
+            factura.FechaCompra = DateTime.Now;
+            DataContextUsers db = HttpContext.RequestServices.GetService(typeof(DataContextUsers)) as DataContextUsers;
+            List<Account> listaUsuarios = db.GetAllAccounts();
+            foreach (Account a in listaUsuarios)
+            {
+                if (a.Usuario == usuario)
+                {
+                    factura.Account = a;
+                }
+            }
+            return View("Checkout", model: factura);
         }
 
     }
